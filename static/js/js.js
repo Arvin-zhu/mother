623a3563d06c0de781336c1e3a75c83f35b2cddb
ArvinZhu <964505072@qq.com>
2016-09-05 15:06:08 +0800

    ok1

--- static/js/js.js
+++ static/js/js.js
@@ -40,6 +40,167 @@ function container4(){
 		freeMode: true
 	})
 }
+function container6(){
+	var arr=$("ul>li");
+	var len=arr.length;
+	var w=$("ul")[0].offsetWidth;
+	var preX,curX;
+	var transferX=0;
+	for(var i=0;i<len;i++){
+		$(arr[i]).css("left",i*w);
+		arr[i].addEventListener("touchstart",touchstart,false);
+		arr[i].addEventListener("touchmove",touchmove,false);
+		arr[i].addEventListener("touchend",touchend,false);
+	}
+	$(".right").on("click",slide_right);
+	$(".left").on("click",slide_left);
+	function slide_right(){
+		event.preventDefault();
+		for(var i=0;i<len;i++){
+			if($(arr[i]).css("left")=="0px"){
+				var $this=i;
+			}
+		}
+		for(var i=0; i<len;i++){
+			if($(arr[len-1]).css("left")=="0px"){
+				for(var i=0;i<len;i++){
+					$(arr[i]).animate({"left":i*w},400);
+				}
+			}else{
+				$(arr[i]).animate({"left":(i-$this-1)*w},400);
+			}
+		}
+	}
+	function slide_left(){
+		event.preventDefault();
+		for(var i=0;i<len;i++){
+			if($(arr[i]).css("left")=="0px"){
+				var $this=i;
+			}
+		}
+		if($(arr[0]).css("left")=="0px"){
+			for(var i=0;i<len;i++){
+				$(arr[i]).animate({"left":w*i},400);
+			}
+		}else{
+			for(var i=0; i<len;i++){
+				$(arr[i]).animate({"left":(i-$this+1)*w},400);
+			}
+		}
+	}
+	//触摸开始
+	function touchstart(event){
+		event.preventDefault();
+		if(event.targetTouches.length==1){      //避免多手指触摸情况
+			var touch=event.targetTouches[0];
+			preX=touch.pageX;
+			preY=touch.pageY;
+		}
+	}
+	$("ul>li").mousedown(function(event){
+		event.preventDefault();
+		preX=event.pageX;
+		$(this).mousemove(function(event){
+			event.preventDefault();               //避免触发默认行为，特别在微信端；
+			var $count=$(this);
+			var $index2=$count.index();
+			curX=event.pageX;
+			transferX +=curX-preX;            //用累加是因为css的样式会叠加！！
+			for(var i=0;i<len;i++){
+				$(arr[i]).css("left",(i-$index2)*w+transferX);
+			}
+			preX=curX;
+		})
+	})
+	$("ul>li").mouseup(function(event){
+		event.preventDefault();
+		$("ul>li").unbind("mousemove");
+		transferX=0;                         //止transferX在下一次触摸中继续累加，导致下一次滑动幅度越来越大；
+		var $this=$(this),
+			$index=$(this).index(),
+			$left1=parseInt($(this).css("left")),
+			$left=Math.abs(parseInt($(this).css("left")));
+		if($left<80){                         //设置手指滑动的距离，当小于80px的时候位置不变；
+			for(var i=0;i<len;i++){
+				var curL=parseInt($(arr[i]).css("left"));
+				$(arr[i]).animate({"left":curL-$left1},400);
+			}
+		}else if($left>80){                  //当滑动的距离大于80px的时候，切换；
+			if($left1>0){                    //当手指向右滑动的时候；
+				if($index!=0){              //判断是不是第一张，如果不是则切换；
+					for(var i=0;i<len;i++){
+						$(arr[i]).animate({"left":(i-$index+1)*w},400);
+					}
+				}else{                         //如果是第一张，则保持不变；
+					for(var i=0;i<len;i++){
+						$(arr[i]).animate({"left":i*w},400);
+					}
+				}
+			}else{                          //当手指向左滑动的时候；
+				if($index!=(len-1)){        //判断是不是最后一张，如果不是则切换；
+					for(var i=0;i<len;i++){
+						$(arr[i]).animate({"left":(i-$index-1)*w},400);
+					}
+				}else{                    //如果是最后一张，则保持位置不变；
+					for(var i=0;i<len;i++){
+						$(arr[i]).animate({"left":(i-$index)*w},400);
+					}
+				}
+			}
+		}
+	})
+	//手指拖动
+	function touchmove(event){
+		event.preventDefault();               //避免触发默认行为，特别在微信端；
+		if(event.targetTouches.length==1){    //避免多手机触摸情况
+			var $count=$(this);
+			var $index2=$count.index();
+			var touch=event.targetTouches[0];
+			curX=touch.pageX;
+			transferX +=curX-preX;            //用累加是因为css的样式会叠加！！
+			for(var i=0;i<len;i++){
+				$(arr[i]).css("left",(i-$index2)*w+transferX);
+			}
+			preX=curX;
+		}
+	}
+	function touchend(event){
+		event.preventDefault();
+		transferX=0;                         //为了防止transferX在下一次触摸中继续累加，导致下一次滑动幅度越来越大；
+		var $this=$(this),
+			$index=$(this).index(),
+			$left1=parseInt($(this).css("left")),
+			$left=Math.abs(parseInt($(this).css("left")));
+		if($left<80){                         //设置手指滑动的距离，当小于80px的时候位置不变；
+			for(var i=0;i<len;i++){
+				var curL=parseInt($(arr[i]).css("left"));
+				$(arr[i]).animate({"left":curL-$left1},400);
+			}
+		}else if($left>80){                  //当滑动的距离大于80px的时候，切换；
+			if($left1>0){                    //当手指向右滑动的时候；
+				if($index!=0){              //判断是不是第一张，如果不是则切换；
+					for(var i=0;i<len;i++){
+						$(arr[i]).animate({"left":(i-$index+1)*w},400);
+					}
+				}else{                         //如果是第一张，则保持不变；
+					for(var i=0;i<len;i++){
+						$(arr[i]).animate({"left":i*w},400);
+					}
+				}
+			}else{                          //当手指向左滑动的时候；
+				if($index!=(len-1)){        //判断是不是最后一张，如果不是则切换；
+					for(var i=0;i<len;i++){
+						$(arr[i]).animate({"left":(i-$index-1)*w},400);
+					}
+				}else{                    //如果是最后一张，则保持位置不变；
+					for(var i=0;i<len;i++){
+						$(arr[i]).animate({"left":(i-$index)*w},400);
+					}
+				}
+			}
+		}
+	}
+}
 function container3(){
 	var map = new AMap.Map("wrap", {
 				resizeEnable: true
@@ -65,20 +226,185 @@ function container5(){
 	})
 }
 function container9(){
-	$(".menu-call").on("click",function(){
-		$(".call").css("display","block");
-		$(".zhezhao").css("display","block");
+	var time=setInterval(autogo,2000),
+		arr=$(".lunbo>li"),
+		arr2=$(".count li"),
+		len=arr.length,
+		w=$("ul")[0].offsetWidth,
+		preX,curX,
+		transferX= 0;
+	for(var i=0;i<len;i++){
+		$(arr[i]).css("left",i*w);
+		arr[i].addEventListener("touchstart",touchstart,false);
+		arr[i].addEventListener("touchmove",touchmove,false);
+		arr[i].addEventListener("touchend",touchend,false);
+	}
+	for(var i=0;i<len;i++){
+		arr2[i].addEventListener("touchstart",countchange,false);
+	}
+	//点击轮播下方圆形切换
+	function countchange(){
+		clearInterval(time);
+		var count_index=$(this).index();
+		$(this).addClass("active").siblings().removeClass("active");
+		for(var i=0;i<len;i++){
+			$(arr[i]).animate({"left":(i-count_index)*w},300);
+		}
+		time=setInterval(autogo,2000);
+	}
+	//触摸开始
+	function touchstart(event){
+		event.preventDefault();
+		clearInterval(time);
+		if(event.targetTouches.length==1){      //避免多手指触摸情况
+			var touch=event.targetTouches[0];
+			preX=touch.pageX;
+		}
+	}
+	$(".lunbo li").mousedown(function(event){
+		clearInterval(time);
+		event.preventDefault();
+		preX=event.pageX;
+		$(this).mousemove(function(event){
+			event.preventDefault();               //避免触发默认行为，特别在微信端；
+			var $count=$(this);
+			var $index2=$count.index();
+			curX=event.pageX;
+			transferX +=curX-preX;            //用累加是因为css的样式会叠加！！
+			for(var i=0;i<len;i++){
+				$(arr[i]).css("left",(i-$index2)*w+transferX);
+			}
+			preX=curX;
+		})
 	})
-	$(".menu-guide").on("click",function(){
-		$(".guide").css("display","block");
-		$(".zhezhao").css("display","block");
+	$(".lunbo li").mouseup(function(event){
+		event.preventDefault();
+		$(".lunbo li").unbind("mousemove");
+		transferX=0;                         //止transferX在下一次触摸中继续累加，导致下一次滑动幅度越来越大；
+		var $this=$(this),
+			$index=$(this).index(),
+			$left1=parseInt($(this).css("left")),
+			$left=Math.abs(parseInt($(this).css("left")));
+		if($left<80){                         //设置手指滑动的距离，当小于80px的时候位置不变；
+			for(var i=0;i<len;i++){
+				var curL=parseInt($(arr[i]).css("left"));
+				$(arr[i]).animate({"left":curL-$left1},400);
+			}
+		}else if($left>80){                  //当滑动的距离大于80px的时候，切换；
+			if($left1>0){                    //当手指向右滑动的时候；
+				if($index!=0){              //判断是不是第一张，如果不是则切换；
+					for(var i=0;i<len;i++){
+						$(arr[i]).animate({"left":(i-$index+1)*w},400);
+					}
+					$(".count li").eq($index-1).addClass("active").siblings().removeClass("active");
+				}else{                         //如果是第一张，则保持不变；
+					for(var i=0;i<len;i++){
+						$(arr[i]).animate({"left":i*w},400);
+					}
+				}
+			}else{                          //当手指向左滑动的时候；
+				if($index!=(len-1)){        //判断是不是最后一张，如果不是则切换；
+					for(var i=0;i<len;i++){
+						$(arr[i]).animate({"left":(i-$index-1)*w},400);
+						$(".count li").eq($index+1).addClass("active").siblings().removeClass("active");
+					}
+				}else{                    //如果是最后一张，则保持位置不变；
+					for(var i=0;i<len;i++){
+						$(arr[i]).animate({"left":(i-$index)*w},400);
+					}
+				}
+			}
+		}
+		time=setInterval(autogo,2000);
 	})
-	$(".menu-message").on("click",function(){
-		$(".message").css("display","block");
-		$(".zhezhao").css("display","block");
-	})
-	$("button").on("click",function(){
-		$(this).parent().css("display","none");
-		$(".zhezhao").css("display","none");
-	})
+	//手指拖动
+	function touchmove(event){
+		event.preventDefault();               //避免触发默认行为，特别在微信端；
+		if(event.targetTouches.length==1){    //避免多手机触摸情况
+			var $count=$(this);
+			var $index2=$count.index();
+			var touch=event.targetTouches[0];
+			curX=touch.pageX;
+			transferX +=curX-preX;            //用累加是因为css的样式会叠加！！
+			for(var i=0;i<len;i++){
+				$(arr[i]).css("left",(i-$index2)*w+transferX);
+			}
+			preX=curX;
+		}
+	}
+	function touchend(event){
+		event.preventDefault();
+		transferX=0;                         //为了防止transferX在下一次触摸中继续累加，导致下一次滑动幅度越来越大；
+		var $this=$(this),
+			$index=$(this).index(),
+			$left1=parseInt($(this).css("left")),
+			$left=Math.abs(parseInt($(this).css("left")));
+		if($left<80){                         //设置手指滑动的距离，当小于80px的时候位置不变；
+			for(var i=0;i<len;i++){
+				var curL=parseInt($(arr[i]).css("left"));
+				$(arr[i]).animate({"left":curL-$left1},400);
+			}
+		}else if($left>80){                  //当滑动的距离大于80px的时候，切换；
+			if($left1>0){                    //当手指向右滑动的时候；
+				if($index!=0){              //判断是不是第一张，如果不是则切换；
+					for(var i=0;i<len;i++){
+						$(arr[i]).animate({"left":(i-$index+1)*w},400);
+					}
+					$(".count li").eq($index-1).addClass("active").siblings().removeClass("active");
+				}else{                         //如果是第一张，则保持不变；
+					for(var i=0;i<len;i++){
+						$(arr[i]).animate({"left":i*w},400);
+					}
+				}
+			}else{                          //当手指向左滑动的时候；
+				if($index!=(len-1)){        //判断是不是最后一张，如果不是则切换；
+					for(var i=0;i<len;i++){
+						$(arr[i]).animate({"left":(i-$index-1)*w},400);
+						$(".count li").eq($index+1).addClass("active").siblings().removeClass("active");
+					}
+				}else{                    //如果是最后一张，则保持位置不变；
+					for(var i=0;i<len;i++){
+						$(arr[i]).animate({"left":(i-$index)*w},400);
+					}
+				}
+			}
+		}
+		time=setInterval(autogo,2000);
+	}
+	//自动轮播效果
+	function autogo(){
+		for(var i=0;i<len;i++){
+			if(parseInt($(arr[i]).css("left"))==0){
+				var this_index=i;
+				break;
+			}
+		}
+		if(this_index!=len-1){
+			for(var i=0;i<len;i++) {
+				$(".count li").eq(this_index+1).addClass("active").siblings().removeClass("active");
+				$(arr[i]).animate({"left": (i - this_index - 1) * w}, 300);
+			}
+		}else{
+			for(var i=0;i<len;i++){
+				$(".count li").eq(0).addClass("active").siblings().removeClass("active");
+				$(arr[i]).animate({"left":i*w},300);
+			}
+		}
+	}
+		$(".menu-call").on("click",function(){
+			$(".call").css("display","block");
+			$(".zhezhao").css("display","block");
+		})
+		$(".menu-guide").on("click",function(){
+			$(".guide").css("display","block");
+			$(".zhezhao").css("display","block");
+		})
+		$(".menu-message").on("click",function(){
+			$(".message").css("display","block");
+			$(".zhezhao").css("display","block");
+		})
+		$("button").on("click",function(){
+			$(this).parent().css("display","none");
+			$(".zhezhao").css("display","none");
+		})
 }
